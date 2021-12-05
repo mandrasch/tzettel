cccff2bc49e8433864ffdf5b856e1a60242d0fab
mandrasch <777278+mandrasch@users.noreply.github.com>
2021-12-05 21:19:45 +0000

    Deploying to gh-pages from @ mandrasch/tzettel@8ed2858aed0dd814e590c0f0b809f43c6b54a47e 🚀

---
+++ _app/pages/about.svelte-d1ad6eb8.js
@@ -0,0 +1,5 @@
+import{S as r,i as n,s as i,t as l,g as h,f as c,E as a,d as p}from"../chunks/vendor-b32ae2ae.js";function u(o){let t;return{c(){t=l(`tzettel is a tiny helper for people who work on multiple projects throughout the day. It combines
+the tasks and work minutes for each project (via hashtag) and generates a clean daily report
+summary.`)},l(e){t=h(e,`tzettel is a tiny helper for people who work on multiple projects throughout the day. It combines
+the tasks and work minutes for each project (via hashtag) and generates a clean daily report
+summary.`)},m(e,s){c(e,t,s)},p:a,i:a,o:a,d(e){e&&p(t)}}}class d extends r{constructor(t){super();n(this,t,null,u,i,{})}}export{d as default};
